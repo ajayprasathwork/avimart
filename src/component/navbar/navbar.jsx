622cdb57027b34388233fd7ab73d7eec7e82b546
ajay <122023127+ajayprasathwork@users.noreply.github.com>
2023-02-09 00:54:03 +0530

    nav link

--- src/component/navbar/navbar.jsx
+++ src/component/navbar/navbar.jsx
@@ -1,8 +1,8 @@
 import React from 'react';
 import './navbar.css'
 import Logo from '../../img/logo.png'
-import { AiOutlineDown,AiOutlineSearch } from "react-icons/ai";
-import { HiOutlineLocationMarker } from "react-icons/hi";
+import { AiOutlineDown,AiOutlineSearch,AiOutlineUser,AiOutlineShoppingCart,AiOutlineHeart } from "react-icons/ai";
+import { HiOutlineLocationMarker,HiOutlineMenuAlt2 } from "react-icons/hi";
 
 const navbar = () => {
     return (
@@ -14,7 +14,7 @@ const navbar = () => {
                 </div>
                 <div className='search'>
                     <div className='findstores'>
-                        <HiOutlineLocationMarker/>
+                        <HiOutlineLocationMarker className='icon'/>
                       <p>Find Stores</p>
                       <AiOutlineDown/>
                     </div>
@@ -27,11 +27,42 @@ const navbar = () => {
                     </div>
                 </div>
                 <div className='headicon'>
+                    <div className='headicon-box'>
+                        <AiOutlineUser className='icon'/>
+                      <p>Account</p>
+                    </div>
+                    <div className='headicon-box'>
+                        <AiOutlineHeart className='icon'/>
+                      <p>Wishlist</p>
+                    </div>
+                    <div className='headicon-box'>
+                        <AiOutlineShoppingCart className='icon'/>
+                      <p>Cart</p>
+                    </div>
 
                 </div>
 
             </div>
-            <div className='nav-link'></div>
+            <div className='nav-link'>
+                <div className='nav-link-part1'>
+                    <a><HiOutlineMenuAlt2 className='icon'/></a>
+                    <a>Home</a>
+                    <a>Top Deals</a>
+                    <a>Hot Offers</a>
+                    <a>New arrivals</a>
+                    <a>Blog</a>
+                    <a>Gift Cards</a>
+
+                </div>
+                <div className='nav-link-part2'>
+                 <a>Recently Viewed<AiOutlineDown className='icon'/></a>
+                 <a>Order Tracking<AiOutlineDown className='icon'/></a>
+                 <a>USD $<AiOutlineDown className='icon'/></a>
+                 <a>Hotline: +01 1234 8888</a>
+
+                </div>
+
+            </div>
 
         </div>
     );
