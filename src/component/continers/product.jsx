1ae46589c4e52d6849a560c36c0ea1ec082e04da
ajay <122023127+ajayprasathwork@users.noreply.github.com>
2023-02-11 23:31:58 +0530

    scroll issue

--- src/component/continers/product.jsx
+++ src/component/continers/product.jsx
@@ -1,18 +1,64 @@
-import React from 'react';
+import React,{useRef,useEffect} from 'react';
 import Arrow from '../carouselarrow/productarrow.jsx'
+import Pcard from '../cards/productcard.jsx'
 import './product.css'
-const product = () => {
+const Product = (props) => {
+    const containerRef = useRef(); // <-- (1) create Ref
+    const sliderTimerRef = useRef();
+
+    useEffect(() => {
+        return () => {
+          // clear any running intervals on component unmount
+          clearInterval(sliderTimerRef.current);
+        };
+      }, []);
+
+      function slide(direction) {
+        // clear any previously set intervals and reset scrollCompleted
+        clearInterval(sliderTimerRef.current);
+        let scrollCompleted = 0;
+      
+        sliderTimerRef.current = setInterval(function() {
+          const container = containerRef.current; // <-- (3) access current ref value
+      
+          if (direction === 'left') {
+            container.scrollLeft -= 10; // <-- (4)  Optional Chaining null check
+          } else {
+            container.scrollLeft += 50; // <-- (4)  Optional Chaining null check
+          }
+          scrollCompleted += 10;
+          if (scrollCompleted >= 100) {
+            clearInterval(sliderTimerRef.current);
+          }
+        }, 50);
+      }
+      
+      
     return (
              <div className='pc'>
                 <div className='pc-head'>
+                    <div className='titel'><p>{props.titel}<span>{props.sub}</span></p></div>
+                    <div className='other-links'></div>
 
                 </div>
-                <div className='pci-continer'>
-                 <Arrow/>
+                <div className='pci-continer'  ref={containerRef} >
+                    {/* <div className='pci-continer-item'> */}
+                      <Pcard id="1"/>
+                      <Pcard id="2"/>
+                      <Pcard id="1"/>
+                      <Pcard id="2"/><Pcard id="1"/>
+                      <Pcard id="2"/><Pcard id="1"/>
+                      <Pcard id="2"/><Pcard id="1"/>
+                      <Pcard id="2"/><Pcard id="1"/>
+                      <Pcard id="2"/>
+                      <Pcard id="2"/><Pcard id="1"/>
+                      <Pcard id="6"/>
+                    {/* </div> */}<Arrow slide={()=>{slide()}}/>
+                    
                 </div>
                
              </div>
     );
 };
 
-export default product;
+export default Product;
